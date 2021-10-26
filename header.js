3565fe17e83116d371a2874059058bdf69d0430a
BillGun89 <90799502+BillGun89@users.noreply.github.com>
2021-10-26 17:40:53 +0200

    Update header.js

--- header.js
+++ header.js
@@ -12,7 +12,6 @@ function header(){
     html += '    <div id="test">';
     html += '        <nav>';
     html += '            <ul>';
-    html += '            <li><a href="index.html">Home</a></li>';
     html += '            <li><a href="gallery.html">Gallery</a></li>';
     html += '            <li><a href="webshop.html">Webshop</a></li>';
     html += '            <li><a href="about.html">About</a></li>';
